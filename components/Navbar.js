62930a0c4d23186f891620ee03506fa879d6e99d
Sithichok6310450735 <Sithichok.k@ku.th>
2023-05-30 10:17:22 +0700

    change dev is Box

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Box } from "@mui/material";
 export default function Navbar() {
   return (
     <nav>
-      <div className="logo">
+      <Box className="logo">
         <Link href={"/"}>
           <Image src="/logo.png" width={50} height={50} alt="logo" />
         </Link>
-      </div>
+      </Box>
       <Link href={"/"}>หน้าแรก</Link>
       <Link href={"/about"}>เกี่ยวกับเรา</Link>
       <Link href={"/products"}>สินค้าทั้งหมด</Link>
